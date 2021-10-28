d5c3f156770b41f9351e71e68ab7a2852a31b2c4
Ruwevcedt <yjs96624@gmail.com>
2021-10-28 21:20:34 +0900

    Add files via upload

--- Nature.py
+++ Nature.py
@@ -6,16 +6,16 @@ from Location import Location
 
 class Nature:
     deck: Location
-    excepted: Location
+    exception: Location
 
     def __init__(self):
         print('\nlocation: generating nature')
         self.deck = Location(suit=None, name='deck', visible_to=[None], content=[])
-        self.excepted = Location(suit=None, name='excepted', visible_to=ALL_MARK, content=[])
+        self.exception = Location(suit=None, name='excepted', visible_to=ALL_MARK, content=[])
         print('location: nature generated: ', self)
 
     def __repr__(self):
-        return f"MOTHER NATURE\ndeck: {self.deck.content}\nexcepted: {self.excepted.content}"
+        return f"MOTHER NATURE\ndeck: {self.deck.content}\nexcepted: {self.exception.content}"
 
     def deck_shuffle(self):
         random.shuffle(self.deck.content)
