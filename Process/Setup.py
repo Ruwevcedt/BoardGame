8f0708342845211e436898a7ca8dcd2b88afed22
Ruwevcedt <yjs96624@gmail.com>
2022-03-05 01:25:25 +0900

    Add files via upload

---
+++ Process/Setup.py
@@ -0,0 +1,53 @@
+from Definition.Card import AllCard, VALID_MARK
+from Definition.Game import Game
+
+
+class Setup:
+    def __call__(self, game: Game):
+        self._deck_makeup(game=game)
+        self._distribute_king(game=game)
+        self._shuffle_deck(game=game)
+        self._distribute_hands(game=game)
+        while not self._check_nation_can_start(game=game):
+            self._make_nation_can_start(game=game)
+        self._shuffle_deck(game=game)
+
+    def _deck_makeup(self, game: Game):
+        game.nature.deck.put_cards(cards=AllCard())
+
+    def _distribute_king(self, game: Game):
+        _kings = game.nature.deck.pop_by_indexes(indexes=game.nature.deck.search_by_letter(letter='K'))
+        for king in _kings:
+            game.search_nation_by_suit(king.suit).castle.king = king
+
+    def _shuffle_deck(self, game: Game):
+        game.nature.deck.shuffle()
+
+    def _distribute_hands(self, game: Game):
+        for i in range(5):
+            for suit in VALID_MARK:
+                for nation in game.search_nation_by_suit(suit=suit):
+                    nation.castle.hands.put_cards(game.nature.deck.draw(quantity=1))
+
+    def _check_nation_can_start(self, game: Game) -> bool:
+        _ = []
+        for suit in VALID_MARK:
+            for nation in game.search_nation_by_suit(suit=suit):
+                _.append(nation.can_start_game())
+        return False if False in _ else True
+
+    def _make_nation_can_start(self, game: Game):
+        for suit in VALID_MARK:
+            for nation in game.search_nation_by_suit(suit=suit):
+                if not nation.can_start_game():
+                    _ = nation.castle.hands.pop_by_indexes(
+                        indexes=nation.castle.hands.search_by_suit(suit=nation.suit)
+                    )
+                    game.nature.deck.put_cards(
+                        cards=nation.castle.hands.pop_by_indexes(
+                            indexes=list(
+                                range(len(nation.castle.hands.content))
+                            )
+                        )
+                    )
+                    nation.castle.hands.put_cards(cards=_)
