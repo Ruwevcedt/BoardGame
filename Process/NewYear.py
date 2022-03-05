4031b49e6e724cf6f3ce4e565ce65da08b1f0a2e
Ruwevcedt <yjs96624@gmail.com>
2022-03-05 10:37:43 +0900

    Add files via upload

--- Process/NewYear.py
+++ Process/NewYear.py
@@ -1,15 +1,17 @@
 from Definition.Game import Game
-
+from Definition.Card import ALL_MARK
 
 class NewYear:
     def __call__(self, game: Game):
         self._draw_cards(game=game)
 
     def _draw_cards(self, game: Game):
+        _quantity = 0
         for suit in game.turn:
             nation = game.search_nation_by_suit(suit=suit)
-            nation.castle.hands.put_cards(
-                game.nature.deck.draw(
-                    quantity=2 if nation.castle.cabinet.light.content[0].letter == 'Q' else 1
-                )
-            )
+            if nation.castle.cabinet.light.content[0].letter == 'Q' or \
+                (nation.castle.cabinet.dark.content[0].letter == 'Q' and nation.castle.cabinet.dark.visible_to == ALL_MARK):
+                _quantity = 2
+            else:
+                _quantity = 1
+            nation.castle.hands.put_cards(game.nature.deck.draw(quantity=_quantity))
