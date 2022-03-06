fc52b8d45bb6079f601b1ba19b3f67d99a87cb83
Ruwevcedt <yjs96624@gmail.com>
2022-03-07 01:56:58 +0900

    Add files via upload

--- Definition/Nation.py
+++ Definition/Nation.py
@@ -13,7 +13,8 @@ class Nation:
         self.field.__init__(suit=self.suit)
 
     def can_start_game(self) -> bool:
-        return True if len(self.castle.hands.search_by_suit(suit=self.suit)) >= 2 else False
+        return True if len(self.castle.hands.search_by_suit(suit=self.suit)) >= 2 and \
+                       len(self.castle.hands.cards) == 5 else False
 
     def is_it_alive(self) -> bool:
         return False if not self.castle.king else True
