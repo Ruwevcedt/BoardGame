dc9b1d21120ba5198776e180b2bcfdef0e7a930e
Ruwevcedt <yjs96624@gmail.com>
2022-03-08 13:56:13 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Definition.Game import Game
-from Process import Setup, NewYear, CabinetReshuffle, Diplomacy
+from Phase import Setup, NewYear, CabinetReshuffle, Diplomacy
 
 test = Game(game_id=0)
-Setup.Setup(game=test)
-NewYear.NewYear(game=test)
+Setup.Setup(game=test).setup()
+NewYear.NewYear(game=test).new_year()
+print('\n')
+CabinetReshuffle.CabinetReshuffle(game=test).cabinet_reshuffle()
+Diplomacy.Diplomacy(game=test)
+print('\n')
